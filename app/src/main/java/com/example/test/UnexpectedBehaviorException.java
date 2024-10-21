31c2b6f288a74c10edcbdf3484cfec0d49dc6c21
FELIX6402 <trikidisfelinis@gmail.com>
2024-10-21 16:17:49 +0300

    i have error with the style view

---
+++ app/src/main/java/com/example/test/UnexpectedBehaviorException.java
@@ -0,0 +1,7 @@
+package com.example.test;
+
+public class UnexpectedBehaviorException extends RuntimeException {
+    public UnexpectedBehaviorException(String message) {
+        super(message);
+    }
+}
